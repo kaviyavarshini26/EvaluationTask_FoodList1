19579b6732eac30febe63ab69f61717146d01a6a
kaviyavarshini26 <131649247+kaviyavarshini26@users.noreply.github.com>
2023-06-01 10:07:50 +0530

    Update SavedFragment.java

--- app/src/main/java/com/example/foodlist/SavedFragment.java
+++ app/src/main/java/com/example/foodlist/SavedFragment.java
@@ -1,9 +1,12 @@
 package com.example.foodlist;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -12,6 +15,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
 
 import com.example.foodlist.AppDatabase;
 import com.example.foodlist.ItemDao;
@@ -19,13 +23,18 @@ import com.example.foodlist.ItemEntity;
 import com.example.foodlist.MyApp;
 import com.example.foodlist.R;
 import com.example.foodlist.SavedAdapter;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class SavedFragment extends Fragment {
+public class SavedFragment extends Fragment  {
 
     RecyclerView recyclerView;
-    SavedAdapter adapter;
+     SavedAdapter adapter;
+//    private List<String> savedItems = new ArrayList<>();
+    private ItemDao itemDao;
+    private AppDatabase database;
 
     public SavedFragment() {
         // Required empty public constructor
@@ -38,9 +47,19 @@ public class SavedFragment extends Fragment {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         adapter = new SavedAdapter(getActivity());
-        recyclerView.setAdapter(adapter);
 
-      //  loadSavedItems();
+        recyclerView.setAdapter(adapter);
+        loadSavedItems();
+
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        database= Room.databaseBuilder(getActivity(),AppDatabase.class,"app_database")
+                .allowMainThreadQueries().fallbackToDestructiveMigration().build();
+        itemDao = database.itemDao();
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -48,10 +67,8 @@ public class SavedFragment extends Fragment {
         return view;
     }
 
+
     private void loadSavedItems() {
-        AppDatabase database = ((MyApp) requireActivity().getApplication()).getDatabase();
-        ItemDao itemDao = database.itemDao();
-        List<Category> remoteList =  itemDao.getAllItems();
         itemDao.getAllItems().observe(getViewLifecycleOwner(), new Observer<List<ItemEntity>>() {
             @Override
             public void onChanged(List<ItemEntity> items) {
@@ -59,5 +76,8 @@ public class SavedFragment extends Fragment {
                 adapter.notifyDataSetChanged();
             }
         });
+
+
     }
 }
+
