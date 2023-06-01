f0a3eff42e5a9876e0f814da6cbce9700dad2e19
kaviyavarshini26 <131649247+kaviyavarshini26@users.noreply.github.com>
2023-06-01 10:07:15 +0530

    Update SavedAdapter.java

--- app/src/main/java/com/example/foodlist/SavedAdapter.java
+++ app/src/main/java/com/example/foodlist/SavedAdapter.java
@@ -4,20 +4,24 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.foodlist.ItemEntity;
-import com.example.foodlist.R;
 
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
 import java.util.List;
+
 
 public class SavedAdapter extends RecyclerView.Adapter<SavedAdapter.ViewHolder> {
 
     private Context context;
-    private List<ItemEntity> items;
+    private List<ItemEntity> items=new ArrayList<>();
+
 
     public SavedAdapter(Context context) {
         this.context = context;
@@ -26,6 +30,8 @@ public class SavedAdapter extends RecyclerView.Adapter<SavedAdapter.ViewHolder> 
     public void setItems(List<ItemEntity> items) {
         this.items = items;
     }
+
+
 
     @NonNull
     @Override
@@ -38,20 +44,34 @@ public class SavedAdapter extends RecyclerView.Adapter<SavedAdapter.ViewHolder> 
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         ItemEntity item = items.get(position);
         holder.itemNameTextView.setText(item.getItemName());
+        holder.itemDescTextView.setText(item.getItemDesc());
+        Glide.with(context)
+
+                .load(item.getItemImage())
+
+                .into(holder.itemImageView);
+
     }
+
+
+
 
     @Override
     public int getItemCount() {
-        return items != null ? items.size() : 0;
+        return  items.size();
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView itemNameTextView;
+        TextView itemDescTextView;
+        ImageView itemImageView;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             itemNameTextView = itemView.findViewById(R.id.item_name_text);
+            itemDescTextView=itemView.findViewById(R.id.item_name_desc);
+            itemImageView=itemView.findViewById(R.id.item_name_image);
         }
     }
 }
